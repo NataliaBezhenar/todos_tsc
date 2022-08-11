bb3282e7c94052f1d6bf07c0feae1b336e717e91
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-08-11 11:57:48 +0200

    initial project setup

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,30 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import HomeView from "../../views/HomeView";
+import TodosView from "../../views/TodosView";
+//import NotFoundView from "../../views/NotFoundView";
+
+interface INavProps {
+  isLoggedIn: boolean;
+}
+
+const Navigation: React.FC<INavProps> = ({ isLoggedIn }) => (
+  <>
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route exact path="/todos">
+        {!isLoggedIn ? <Redirect to="/" /> : <TodosView />}
+      </Route>
+    </Switch>
+  </>
+);
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.user.username.isLoggedIn,
+});
+
+export default connect(mapStateToProps, null)(Navigation);
+
+{
+  /* <Route component={NotFoundView} /> */
+}
