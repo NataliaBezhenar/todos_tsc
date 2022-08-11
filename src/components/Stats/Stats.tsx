5b91be1e16d2774df7b5f96d536031058d68c957
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-08-11 16:58:07 +0200

    refactoring

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
+import { RootState } from "../../redux/store";
 import styles from "./Stats.module.css";
 
-const Stats = ({ total, completed, incompleted }) => (
+interface IStats {
+  total: number;
+  completed: number;
+  incompleted: number;
+}
+
+const Stats: React.FC<IStats> = ({ total, completed, incompleted }) => (
   <div className={styles.stats}>
     <p className={styles.stats__item}>
       <span className={styles.stats__value}>{total}</span>
@@ -19,16 +26,28 @@ const Stats = ({ total, completed, incompleted }) => (
   </div>
 );
 
-const getCompeltedTodosCount = (todos) =>
-  todos.reduce((total, todo) => (todo.completed ? total + 1 : total), 0);
+const getCompeltedTodosCount = (todos: []) =>
+  todos.reduce(
+    (
+      total: number,
+      todo: { total: number; completed: number; incompleted: number }
+    ) => (todo.completed ? total + 1 : total),
+    0
+  );
 
-const getIncompeltedTodosCount = (todos) =>
-  todos.reduce((total, todo) => (!todo.completed ? total + 1 : total), 0);
+const getIncompeltedTodosCount = (todos: []) =>
+  todos.reduce(
+    (
+      total: number,
+      todo: { total: number; completed: number; incompleted: number }
+    ) => (!todo.completed ? total + 1 : total),
+    0
+  );
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: RootState) => ({
   total: state.todos.items.length,
   completed: getCompeltedTodosCount(state.todos.items),
   incompleted: getIncompeltedTodosCount(state.todos.items),
 });
 
-export default connect(mapStateToProps)(Stats);
+export default connect(mapStateToProps, null)(Stats);
